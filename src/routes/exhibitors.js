d31f99453c37bb2eb5ddacd6e814bfc9f90788c2
Sandhyaravoor <sandhyaravoor.25@gmail.com>
2026-01-30 13:31:15 +0530

    update

--- src/routes/exhibitors.js
+++ src/routes/exhibitors.js
@@ -6,10 +6,13 @@ router.get('/', exhibitorController.getAllExhibitors);
 router.get('/stats', exhibitorController.getExhibitorStats);
 router.get('/:id', exhibitorController.getExhibitor);
 
-// Protected routes - no authentication for now to debug
+// Protected routes
 router.post('/', exhibitorController.createExhibitor);
 router.put('/:id', exhibitorController.updateExhibitor);
 router.delete('/:id', exhibitorController.deleteExhibitor);
 router.post('/bulk/update-status', exhibitorController.bulkUpdateStatus);
+
+// Add resend credentials route
+router.post('/:id/resend-credentials', exhibitorController.resendCredentials);
 
 module.exports = router;
