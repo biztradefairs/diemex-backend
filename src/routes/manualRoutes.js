af99dddb3e06eb87165a000ed451be7d4b64e10c
Sandhyaravoor <sandhyaravoor.25@gmail.com>
2026-02-16 14:31:08 +0530

    update

--- src/routes/manualRoutes.js
+++ src/routes/manualRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const manualController = require('../controllers/ManualController');
+const manualController = require('../controllers/manualController');
 const { authenticate, authorize } = require('../middleware/auth');
 
 // ======================
