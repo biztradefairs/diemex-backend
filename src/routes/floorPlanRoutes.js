36cea542fd343e21b812fa99239e1e7506b3b0cb
Sandhyaravoor <sandhyaravoor.25@gmail.com>
2026-02-28 11:20:37 +0530

    update

--- src/routes/floorPlanRoutes.js
+++ src/routes/floorPlanRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const BoothService = require('../services/BoothService');
+const BoothService = require('../services/FloorPlanService');
 
 // Configure multer with memory storage and file size limit
 const upload = multer({ 
