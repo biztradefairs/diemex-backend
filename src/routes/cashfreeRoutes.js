0869745fc3e160c23c6c4fb7da3bcaa22b5499a1
rohan654321 <mondalrohan201@gmail.com>
2026-04-06 17:41:54 +0530

    update

--- src/routes/cashfreeRoutes.js
+++ src/routes/cashfreeRoutes.js
@@ -39,7 +39,17 @@ const verifyWebhookSignature = (body, signature, secretKey) => {
     return false;
   }
 };
-
+router.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.diemex.in');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
 // Webhook for payment status updates
 router.post('/webhook', async (req, res) => {
   try {
